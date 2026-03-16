45327e56e722ef3a7a18bb7b075876886870b663
gabychares <gchavez27@alumnos.uaq.mx>
2026-03-16 02:38:21 -0600

    Empecé a crear una función para el punto 4.2, hasta ahora solo calcula el índice de Simpson

--- 01_scripts/script1.R
+++ 01_scripts/script1.R
@@ -1,7 +1,6 @@
 
 ## Cargar los datos
-datos <- read.csv("02_rawdata/diversidad - Hoja 1.csv")
-datos
+diversidad <- read_csv("02_rawdata/diversidad - Hoja 1.csv")
 
 ## Cargar librerias
 install.packages("tidyverse")
@@ -9,6 +8,29 @@ library(tidyverse)
 library(vegan)
 install.packages("iNEXT")
 library(iNEXT)
+
+# Diversidad alfa 
+divalf <- function (x){
+  x -> abun
+  as.numeric(abun) -> abun
+  sum(abun) -> total
+  iS <- function(abun, total){
+    (abun / total)^2 ->suma
+    sum(suma)->simp
+    
+  }
+  simp <- iS (abun, total)
+  return(simp)
+}
+sitio1 <- divalf (diversidad [1:10,2])
+sitio2 <- divalf (diversidad [1:9,4])
+sitio3 <-divalf (diversidad [1:8,6])
+sitio4 <-divalf (diversidad [1,8])
+sitio5 <-divalf (diversidad [1:12,10])
+sitio6 <-divalf (diversidad [1:13,12])
+sitio7 <-divalf (diversidad [1:10,14])
+
+#Divalfa <- data.frame()
 
 
 
