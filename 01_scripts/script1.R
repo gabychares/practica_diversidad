020da319e3bbd8b237b48afd054e36932d79b31e
aluna-bio <aluna54@alumnos.uaq.mx>
2026-03-16 13:17:30 -0600

    subi las tablas de datos, la tabla de las abundancias y la tabla con los datos que hicimos por muestreo.

--- 01_scripts/script1.R
+++ 01_scripts/script1.R
@@ -1,7 +1,7 @@
 
 ## Cargar los datos
-datos <- read.csv("02_rawdata/diversidad - Hoja 1.csv")
-datos
+datos1 <- read.csv("02_rawdata/abundancias - Hoja 1.csv") # son los datos de abundancias 
+datos1
 
 ## Cargar librerias
 install.packages("tidyverse")
@@ -10,6 +10,11 @@ library(vegan)
 install.packages("iNEXT")
 library(iNEXT)
 
+
+## 4.2 Diversidad alfa
+
+sp_matrix <- datos1 %>% column_to_rownames("sitios") # convierte la tabla en matriz, para que los sitios pasen a ser nombres de las filas
+sp_matrix
 
 
 
