ceeeb38ff9dfd8f2df91c086ca7061507ea9b056
aluna-bio <aluna54@alumnos.uaq.mx>
2026-03-16 00:39:55 -0600

    puse las librerias

--- 01_scripts/script1.R
+++ 01_scripts/script1.R
@@ -1,1 +1,16 @@
-read.csv("02_rawdata/diversidad - Hoja 1.csv")
+
+## Cargar los datos
+datos <- read.csv("02_rawdata/diversidad - Hoja 1.csv")
+datos
+
+## Cargar librerias
+install.packages("tidyverse")
+library(tidyverse)
+library(vegan)
+install.packages("iNEXT")
+library(iNEXT)
+
+
+
+
+
