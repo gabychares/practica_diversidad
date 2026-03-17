8489962b3085efa35be185c7d9b466bcd5162d58
gabychares <gchavez27@alumnos.uaq.mx>
2026-03-16 19:37:41 -0600

    Continué con la función para calcular lo que se pide en el punto 4.2. ya solo me faltaría el índice de CHAO1. Calcule los indices para cada sitio Cree una tabla con estos indices y los sitios

--- 01_scripts/script1.R
+++ 01_scripts/script1.R
@@ -1,7 +1,7 @@
 
 ## Cargar los datos
-datos1 <- read.csv("02_rawdata/abundancias - Hoja 1.csv") # son los datos de abundancias (tabla de abundancias )
-datos1
+diveridad <- read.csv("02_rawdata/abundancias - Hoja 1.csv") # son los datos de abundancias (tabla de abundancias )
+
 
 ## Cargar librerias
 install.packages("tidyverse")
@@ -11,29 +11,42 @@ install.packages("iNEXT")
 library(iNEXT)
 
 # Diversidad alfa 
+Results <- c()
 divalf <- function (x){
   x -> abun
   as.numeric(abun) -> abun
-  sum(abun) -> total
-  iS <- function(abun, total){
-    (abun / total)^2 ->suma
-    sum(suma)->simp
-    
+  sum(abun) -> riqueza
+  p <- (abun/riqueza)
+  p <- p[p>0]
+  H <- -(sum)(p*log(p))
+  iS <- function(abun, riqueza){
+    (abun / riqueza)^2 ->suma
+    sum(suma)-> simp
+    }
+  simp <- iS (abun, riqueza)
+  
+  ins <- function(simp){
+    1/simp -> gini
   }
-  simp <- iS (abun, total)
-  return(simp)
+  invs <- igs(simp)
+  pielou <- (1/log(riqueza)) 
+  
+return (c(riqueza,H, simp, invs, pielou))
 }
-sitio1 <- divalf (diversidad [1:10,2])
-sitio2 <- divalf (diversidad [1:9,4])
-sitio3 <-divalf (diversidad [1:8,6])
-sitio4 <-divalf (diversidad [1,8])
-sitio5 <-divalf (diversidad [1:12,10])
-sitio6 <-divalf (diversidad [1:13,12])
-sitio7 <-divalf (diversidad [1:10,14])
 
-#Divalfa <- data.frame()
-
-
-
+# Calcular los diferentes índices para cada sitio
+sitio1 <- divalf (diveridad [1,])
+sitio2 <- divalf (diveridad [2,])
+sitio3 <-divalf (diveridad [3,])
+sitio4 <-divalf (diveridad [4,])
+sitio5 <-divalf (diveridad [5,])
+sitio6 <-divalf (diveridad [6,])
+sitio7 <-divalf (diveridad [7,])
+#Crear una tabla en donde se muestren los diferentes sirios con sus respectivos índices
+Diversidad_alfa <- rbind(sitio1,sitio2,sitio3, sitio4,sitio5,sitio6,sitio7)
+columnas <- c("riqueza","H", "simp", "invs", "pielou")
+colnames(Diversidad_alfa) <- columnas
+#Guardar esta tabla en la carpeta de resultados 
+write.csv (Diversidad_alfa, "03_results/Diversidad_alfa.csv")
 
 
